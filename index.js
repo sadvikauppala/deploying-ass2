7cf7d5e285e9473f0861310f40a024b8c8dfebda
uppala sadvika <uppalasadvika06@gmail.com>
2025-02-23 11:34:57 +0530

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { resolve } = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = 3012;
+
+app.use(express.static('static'));
+
+
+
+app.get('/', (req, res) => {
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
